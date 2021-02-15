1290a2012362bee62df579fab364db02498cab50
evertonesc <evertonsouzaesc@gmail.com>
2021-02-15 20:44:12 -0300

    Feat: Import modules

--- backend/src/adapters/adapters.module.ts
+++ backend/src/adapters/adapters.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CreateStudentUseCase } from 'src/use-cases/core/student/createStudentUseCase';
+import { StudentProvider } from 'src/use-cases/studentProvider';
+import { UseCasesModule } from 'src/use-cases/use-cases.module';
+import { StudentController } from './controllers/studentController';
 
-@Module({})
-export class AdaptersModule {}
+@Module({
+    controllers: [StudentController],
+    imports: [UseCasesModule]
+    //imports: [CreateStudentUseCase],
+    //providers: [CreateStudentUseCase]
+})
+export class AdaptersModule { }

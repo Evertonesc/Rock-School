1316fdb18535dd4103a1dc9aedea28f4be218939
evertonesc <evertonsouzaesc@gmail.com>
2021-02-16 20:19:42 -0300

    Feat: StudentTestData class

--- backend/test/helpers/studentDtoTest.ts
+++ backend/test/helpers/studentDtoTest.ts
@@ -1,12 +1,15 @@
 import { StudentDto } from "src/adapters/dtos/studentDto";
 
-function returnValidStudentDto(): StudentDto {
-    const validStudentDto: StudentDto = {
-        firstName: 'Sasuke',
-        lastName: 'Uchiha',
-        email: 'betrayer@akatsuki.com.br',
-        birthDate: new Date(1995, 6, 18),
-        instrument: 1,
+export class StudentTestData {
+
+    static createValidDto() {
+        const validStudentDto: StudentDto = {
+            firstName: 'Sasuke',
+            lastName: 'Uchiha',
+            email: 'betrayer@akatsuki.com.br',
+            birthDate: new Date(1995, 6, 18),
+            instrument: 1,
+        }
+        return validStudentDto;
     }
-    return validStudentDto;
 }

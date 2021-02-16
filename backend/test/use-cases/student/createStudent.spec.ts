4c351c23e620c0d8566b74d15d7ff0a24bc07299
evertonesc <evertonsouzaesc@gmail.com>
2021-02-16 20:20:03 -0300

    Feat: Initial use-cases test implementation

--- backend/test/use-cases/student/createStudent.spec.ts
+++ backend/test/use-cases/student/createStudent.spec.ts
@@ -1,10 +1,10 @@
-import { IFullName } from "src/domain/interfaces/valueObejcts/fullName.interface"
-import { FullName } from "src/domain/valueObjects/fullName"
-import { CreateStudentUseCase } from "src/use-cases/core/student/createStudentUseCase";
+import { UseCasesFactory } from "test/factories/useCaseFactory";
+import { StudentTestData } from "test/helpers/studentDtoTest";
 
 describe('CreateStudentUseCase', () => {
-    test('Create Student - Should create a student with success...', () => {
-        const fullName: IFullName = FullName.create('Edward', 'Erick');
-        let createStudentUseCase = new CreateStudentUseCase();        
+    test('Create Student - Should create a student with success...', async () => {
+        const studentDto = StudentTestData.createValidDto();
+        const studentUseCase = UseCasesFactory.createStudentUseCase();
+        const useCaseResponse = await studentUseCase.createStudent(studentDto);
     })
 })

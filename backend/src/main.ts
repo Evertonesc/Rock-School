89ae634e51096e83c3899077027a6d9413f3f708
evertonesc <evertonsouzaesc@gmail.com>
2021-02-18 23:01:36 -0300

    Feat: Sequelize Authentication on main #1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { sequelizeAuthentication } from './infra/core/database/database';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  sequelizeAuthentication();
   await app.listen(3000);
 }
 bootstrap();

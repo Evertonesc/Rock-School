929fb75a15d08c7f37167f3cb7f5da69e534e281
evertonesc <evertonsouzaesc@gmail.com>
2021-02-28 23:17:47 -0300

    Refactor: Clear

--- backend/src/adapters/adapters.module.ts
+++ backend/src/adapters/adapters.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CreateStudentUseCase } from 'src/use-cases/core/student/createStudentUseCase';
-import { StudentProvider } from 'src/use-cases/studentProvider';
 import { UseCasesModule } from 'src/use-cases/use-cases.module';
 import { StudentController } from './controllers/studentController';
 
 @Module({
     controllers: [StudentController],
     imports: [UseCasesModule]
-    //imports: [CreateStudentUseCase],
-    //providers: [CreateStudentUseCase]
 })
 export class AdaptersModule { }

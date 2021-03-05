7c1b55ad28c1b2576aefb5f8aea33b7a234b1ee5
evertonesc <evertonsouzaesc@gmail.com>
2021-03-04 22:52:59 -0300

    Refactor: Changing sequelize connection

--- backend/src/infra/factory/sequelizeFactory.ts
+++ backend/src/infra/factory/sequelizeFactory.ts
@@ -1,10 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
-
 export class SequelizeFactory {
-    static createSequelizeClient(database: string, dbUser: string, dbPassword: string, dbHost: string): Sequelize {
-        return new Sequelize(database, dbUser, dbPassword, {
-            host: dbHost,
-            dialect: 'postgres'
-        });
+    static connectPostgresClient(connetionData: any): Sequelize {
+        return new Sequelize(connetionData);
     }
 }

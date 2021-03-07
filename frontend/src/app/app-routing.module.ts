495d3a0709ca7795eb3c39d027277ffd78ae07db
evertonesc <evertonsouzaesc@gmail.com>
2021-03-07 02:30:37 -0300

    Feat: Add login and home routes with lazy-loaded

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(pagesModule => pagesModule.LoginModule) },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(pagesModule => pagesModule.HomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

eb4b4f2b80911e55aa4c54bc8cc533c0d4d115b7
evertonesc <evertonsouzaesc@gmail.com>
2021-03-13 17:28:47 -0300

    Feat: Initial implementation of sigin page

--- frontend/src/app/pages/signin/signin.component.ts
+++ frontend/src/app/pages/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { String } from 'src/app/enums/typesEnums';
+import { SigninFactory } from './factories/signinFactory';
 import { SigninService } from './services/signin.service';
 
 @Component({
@@ -6,11 +9,24 @@ import { SigninService } from './services/signin.service';
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
+
 export class SigninComponent implements OnInit {
 
-  constructor(private signinService: SigninService) { }
+  constructor(private signinService: SigninService, private formBuilder: FormBuilder) { }
+  signin = SigninFactory.createSigninForm();
 
   ngOnInit(): void {
+    this.signin;
   }
 
+  signinForm = this.formBuilder.group({
+    name: [this.signin.name, Validators.required],
+    address: this.signin.address
+  })
+
+  onSubmmit(): void {
+    const nameStudent = this.signinForm.get('name');
+    console.warn('Here is your info', this.signinForm.value);
+    this.signinForm.reset();
+  }
 }
--- frontend/src/app/pages/signin/signin.module.ts
+++ frontend/src/app/pages/signin/signin.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { SigninRoutingModule } from './signin-routing.module';
 import { SigninComponent } from './signin.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [SigninComponent],
   imports: [
     CommonModule,
-    SigninRoutingModule
+    SigninRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class SigninModule { }

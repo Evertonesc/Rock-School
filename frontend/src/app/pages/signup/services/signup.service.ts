2e9139154c8b3e577047f300092ddb50284d6d53
evertonesc <evertonsouzaesc@gmail.com>
2021-03-30 21:41:41 -0300

    Feat: Initial services implementation

--- frontend/src/app/pages/signup/services/signup.service.ts
+++ frontend/src/app/pages/signup/services/signup.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  registerStudent(signupForm: FormGroup) {
+    const signupDto = signupForm.value;
+  }
 }

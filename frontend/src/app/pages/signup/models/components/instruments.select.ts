d1fcc5550a6435e7b4043790f8c7ffb4ea27ac62
evertonesc <evertonsouzaesc@gmail.com>
2021-04-02 18:00:16 -0300

    Feat: Instruments select component

---
+++ frontend/src/app/pages/signup/models/components/instruments.select.ts
@@ -0,0 +1,15 @@
+export interface IInstruments {
+    value: number;
+    viewValue: string
+}
+
+export const instruments: IInstruments[] = [
+    { value: 0, viewValue: 'Guitar' },
+    { value: 1, viewValue: 'Bass' },
+    { value: 2, viewValue: 'Drums' },
+    { value: 3, viewValue: 'Sax' },
+    { value: 4, viewValue: 'Keyboard' },
+    { value: 5, viewValue: 'Piano' },
+    { value: 6, viewValue: 'Cello' },
+    { value: 7, viewValue: 'Violin' }
+  ];

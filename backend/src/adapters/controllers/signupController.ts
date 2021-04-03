47dcafcc272d32b0f5082d2f4aa0b133ec3c0043
evertonesc <evertonsouzaesc@gmail.com>
2021-04-03 18:17:05 -0300

    Refactor: Removing white space from constructor

--- backend/src/adapters/controllers/signupController.ts
+++ backend/src/adapters/controllers/signupController.ts
@@ -1,6 +1,5 @@
-import { Inject, Post } from "@nestjs/common";
+import { Body, Inject, Post, Put } from "@nestjs/common";
 import { Controller } from "@nestjs/common";
-import { CreateStudentUseCase } from "src/use-cases/core/student/createStudentUseCase";
 import { ICreateStudent } from "src/use-cases/interfaces/createStudent.interface";
 import { CreateStudentResponse } from "src/use-cases/types/createStudentResponse";
 import { StudentDto } from "../dtos/studentDto";
@@ -9,12 +8,15 @@ import { StudentDto } from "../dtos/studentDto";
 @Controller('v1/student')
 export class SignUpController {
 
-    constructor(@Inject('ICreateStudent') private createStudentUseCase: ICreateStudent) {
-        //this.createStudentUseCase = createStudentUseCase;
-    }
+    constructor(@Inject('ICreateStudent') private createStudentUseCase: ICreateStudent) { }
 
     @Post()
-    async registerStudent(studentDto: StudentDto): Promise<CreateStudentResponse> {
+    async registerStudent(@Body() studentDto: StudentDto): Promise<CreateStudentResponse> {
         return await this.createStudentUseCase.createStudent(studentDto);
+    }
+
+    @Put()
+    create(@Body() studentDto: StudentDto): string {
+        return JSON.stringify(studentDto);
     }
 }

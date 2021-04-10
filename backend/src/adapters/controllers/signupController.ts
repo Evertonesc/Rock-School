a14184e2cfad10d557e478a480102d8b85cc2e43
evertonesc <evertonsouzaesc@gmail.com>
2021-04-10 18:15:32 -0300

    Refactor: Changing controller return type to standard type created (#14)

--- backend/src/adapters/controllers/signupController.ts
+++ backend/src/adapters/controllers/signupController.ts
@@ -2,17 +2,18 @@ import { Body, Inject, Post, Put } from "@nestjs/common";
 import { Controller } from "@nestjs/common";
 import { StudentDto } from "@/adapters/dtos/studentDto";
 import { ICreateStudent } from "@/use-cases/interfaces/createStudent.interface";
-import { CreateStudentResponse } from "@/use-cases/types/createStudentResponse";
+import { IHttpResponse } from "@/domain/validations/httpResponse";
+import { Student } from "@/domain/entities/student";
 
 
 @Controller('v1/student')
 export class SignUpController {
 
-    constructor(@Inject('ICreateStudent') private createStudentUseCase: ICreateStudent) { }
+    constructor(@Inject('ICreateStudent') private _createStudentUseCase: ICreateStudent) { }
 
     @Post()
-    async registerStudent(@Body() studentDto: StudentDto): Promise<CreateStudentResponse> {
-        return await this.createStudentUseCase.createStudent(studentDto);
+    async registerStudent(@Body() studentDto: StudentDto): Promise<IHttpResponse<Student>> {
+        return await this._createStudentUseCase.createStudent(studentDto);
     }
 
     @Put()

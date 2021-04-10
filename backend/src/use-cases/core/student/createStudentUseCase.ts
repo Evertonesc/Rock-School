f779fc59a3ec6ee98621d6d4c1aafbbadd0f53dd
evertonesc <evertonsouzaesc@gmail.com>
2021-04-10 18:14:49 -0300

    Refactor: Changing the return type from use case (#14)

--- backend/src/use-cases/core/student/createStudentUseCase.ts
+++ backend/src/use-cases/core/student/createStudentUseCase.ts
@@ -1,27 +1,29 @@
 import { Injectable } from "@nestjs/common";
 
 import { StudentDto } from "@/adapters/dtos/studentDto";
-import { CreateStudentError } from "@/domain/errors/createStudentError";
 import { IEmail } from "@/domain/interfaces/valueObejcts/email.interface";
 import { IFullName } from "@/domain/interfaces/valueObejcts/fullName.interface";
 import { Email } from "@/domain/valueObjects/email";
 import { FullName } from "@/domain/valueObjects/fullName";
 import { ICreateStudent } from "@/use-cases/interfaces/createStudent.interface";
 import { CreateStudentResponse } from "@/use-cases/types/createStudentResponse";
+import { IHttpResponse, ResponseFactory } from "@/domain/validations/httpResponse";
+import { Student } from "@/domain/entities/student";
 
 @Injectable()
 export class CreateStudentUseCase implements ICreateStudent {
 
-    async createStudent(studentDto: StudentDto): Promise<CreateStudentResponse> {
+    async createStudent(studentDto: StudentDto): Promise<IHttpResponse<Student>> {
         const studentName: IFullName = FullName.create(studentDto.firstName, studentDto.lastName);
         if (studentName.validate() == false)
-            return new CreateStudentError.InvalidFullName;
+            return ResponseFactory.badRequest(false, `The email ${studentDto.email} is invalid`);
 
         const studentEmail: IEmail = Email.create(studentDto.email);
         if (studentEmail.validate() == false)
-            return new CreateStudentError.InvalidEmail(studentEmail.address);
+            return ResponseFactory.badRequest(false, `The email ${studentDto.email} is invalid`);
     }
-    updateStudent(studentDto: StudentDto): Promise<CreateStudentResponse> {
+
+    updateStudent(studentDto: StudentDto): Promise<IHttpResponse<Student>> {
         throw new Error("Method not implemented.");
     }
     checkIfStudentAlreadyExists(studentDto: StudentDto): Promise<CreateStudentResponse> {

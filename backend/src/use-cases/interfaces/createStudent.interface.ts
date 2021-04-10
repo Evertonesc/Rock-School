58db67637342fdf90da21b23968753f01b4a2ec5
evertonesc <evertonsouzaesc@gmail.com>
2021-04-10 18:10:52 -0300

    Refactor: Change return type from create student interface

--- backend/src/use-cases/interfaces/createStudent.interface.ts
+++ backend/src/use-cases/interfaces/createStudent.interface.ts
@@ -1,10 +1,12 @@
 import { StudentDto } from "@/adapters/dtos/studentDto";
+import { Student } from "@/domain/entities/student";
+import { IHttpResponse } from "@/domain/validations/httpResponse";
 import { CreateStudentResponse } from "../types/createStudentResponse";
 
 export interface ICreateStudent {
-    createStudent(studentDto: StudentDto): Promise<CreateStudentResponse>
+    createStudent(studentDto: StudentDto): Promise<IHttpResponse<Student>>
 
-    updateStudent(studentDto: StudentDto): Promise<CreateStudentResponse>
+    updateStudent(studentDto: StudentDto): Promise<IHttpResponse<Student>>
 
     checkIfStudentAlreadyExists(studentDto: StudentDto): Promise<CreateStudentResponse>
 }

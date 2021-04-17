353c34ce008c5cf984f215f4f44faa3b7ff7b030
evertonesc <evertonsouzaesc@gmail.com>
2021-04-16 23:23:02 -0300

    Refactor: base controller implementation (#17)

--- backend/src/adapters/presenters/core/base-controller.ts
+++ backend/src/adapters/presenters/core/base-controller.ts
@@ -18,4 +18,39 @@ export abstract class BaseController {
         const codeResponse = res.status(code);
         return codeResponse.json({ message });
     }
+
+    public ok<T>(res: express.Response, dto?: T) {
+        const typeResponse = res.type('application/json');
+        if (!!dto)
+            return res.status(200).json(dto);
+
+        return res.sendStatus(200);
+    }
+
+    public created(res: express.Response) {
+        return res.sendStatus(201);
+    }
+
+    public clienteError(res: express.Response, message?: string) {
+        return BaseController.jsonResponse(res, 400, message ? message : 'Unauthorized');
+    }
+
+    public forbidden(res: express.Response, message?: string) {
+        return BaseController.jsonResponse(res, 403, message ? message : 'Forbidden');
+    }
+
+    public notFound(res: express.Response, message?: string) {
+        return BaseController.jsonResponse(res, 404, message ? message : 'Not Found');
+    }
+
+    public todo(res: express.Response) {
+        return BaseController.jsonResponse(res, 400, 'TODO');
+    }
+
+    public fail(res: express.Response, error: Error | string) {
+        console.log(error);
+        return res.status(500).json({
+            message: error.toString()
+        })
+    }
 }

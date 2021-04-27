ffbec683e297269f5b4dfa1e77f6ecde215b18a5
evertonesc <evertonsouzaesc@gmail.com>
2021-04-27 00:22:02 -0300

    Refactor: Return messages enum

--- backend/src/adapters/presenters/core/http-response.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IHttpResponse<T> {
-    readonly isSuccess: boolean;
-    readonly isFailure: boolean;
-    readonly message: string;
-    readonly body: T;
-}
---
+++ backend/src/adapters/presenters/enums/return-messages.ts
@@ -0,0 +1,3 @@
+export enum StudentMessages {
+    Created = 'Welcome to the Rock School!'
+}

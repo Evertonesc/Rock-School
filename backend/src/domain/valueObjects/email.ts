1f6655a6f30aeee175ee3cc609c76d878469dc74
evertonesc <evertonsouzaesc@gmail.com>
2021-05-10 22:50:20 -0300

    Refactor: Change Email Dto structure validation

--- backend/src/domain/valueObjects/email.ts
+++ backend/src/domain/valueObjects/email.ts
@@ -1,40 +1,45 @@
 import { ValidationResult } from "../core/interfaces/validations/validation-result";
 import { IValidationResult } from "../core/interfaces/validations/validation-result.interface";
 import { IEmail } from "../core/interfaces/valueObejcts/email.interface";
+import { PropertieValidation } from "../validations/propertie-validation";
 
-export class Email implements IEmail {
+export class Email extends PropertieValidation implements IEmail {
     readonly address: string;
 
-    private constructor(address: string) {
+    private constructor(address: string, objectValidation: IValidationResult) {
+
+        super(objectValidation.isValid, objectValidation.message);
+
         this.address = address;
         Object.freeze(this);
     }
 
     static create(address: string) {
-        return new Email(address);
+        const validation = this.validate(address);
+        return new Email(address, validation);
     }
 
-    validate(): IValidationResult {
+    static validate(emailAddress: string): IValidationResult {
         var tester = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 
-        if (!this.address)
+        if (!emailAddress)
             return ValidationResult.create(false, 'The email cannot be empty');
 
-        if (this.address.length > 256)
+        if (emailAddress.length > 256)
             return ValidationResult.create(false, 'The email cannot have more than 256 characters');
 
-        if (tester.test(this.address) === false)
-        return ValidationResult.create(false, `The email ${this.address} is invalid` );
+        if (tester.test(emailAddress) === false)
+            return ValidationResult.create(false, `The email ${emailAddress} is invalid`);
 
-        var [account, address] = this.address.split('@');
+        var [account, emailAddress] = emailAddress.split('@');
         if (account.length > 64)
-        return ValidationResult.create(false, `The email ${this.address} is invalid` );
+            return ValidationResult.create(false, `The email ${emailAddress} is invalid`);
 
-        var domainParts = address.split('.');
+        var domainParts = emailAddress.split('.');
         if (domainParts.some(function (part) {
             return part.length > 63
         })) {
-            return ValidationResult.create(false, `The email ${this.address} is invalid` );
+            return ValidationResult.create(false, `The email ${emailAddress} is invalid`);
         }
 
         return ValidationResult.create(true);
